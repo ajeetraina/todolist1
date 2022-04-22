174b277aad3ef28fe532a233c26427d3687c1d25
Ajeet Singh Raina, Docker Captain, ARM Innovator, RedisInc <ajeetraina@gmail.com>
2022-04-22 11:34:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const port = 3000
 const redis = require('redis');
 
 var app = express();
